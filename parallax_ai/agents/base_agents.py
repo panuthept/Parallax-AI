08ad948a45947b60d0348af45f43c1ec9787f446
Panuthep <falcon_270394@hotmail.co.th>
2025-09-01 18:34:42 +0800

    JSONOutputAgent support parallel tries

--- parallax_ai/agents/base_agents.py
+++ parallax_ai/agents/base_agents.py
@@ -148,8 +148,16 @@ class JSONOutputAgent(Agent):
             return output
         except Exception:
             return None
+        
+    def _inputs_processing(self, inputs):
+        processed_inputs = super()._inputs_processing(inputs)
 
-    def _output_processing(self, input, output) -> dict:
+        duplicated_processed_inputs = []
+        for processed_input in processed_inputs:
+            duplicated_processed_inputs.extend(processed_input * self.max_tries)
+        return duplicated_processed_inputs
+
+    def _output_processing(self, input, output) -> dict|list[dict]:
         output = super()._output_processing(input, output)
         return self.json_parser(output)
 
@@ -157,34 +165,57 @@ class JSONOutputAgent(Agent):
         self, 
         inputs, 
     ) -> List[str]:
-        processed_inputs = self._inputs_processing(inputs)
-        outputs = self.client.run(
-            inputs=processed_inputs,
-            model=self.model,
-        )
-        return [self._output_processing(input, output) for input, output in zip(processed_inputs, outputs)]
+        duplicated_outputs = super().run(inputs)
+        # Take the first valid output from the duplicated outputs
+        outputs = []
+        for i in range(len(inputs)):
+            for j in range(self.max_tries):
+                output = duplicated_outputs[i * self.max_tries + j]
+                if output is not None:
+                    outputs.append(output)
+                    break
+            if len(outputs) == i:
+                # All tries failed, return None
+                outputs.append(None)
+        assert len(outputs) == len(inputs)
+        return outputs
 
     def irun(
         self, 
         inputs, 
     ) -> Iterator[str]:
-        processed_inputs = self._inputs_processing(inputs)
-        for i, output in enumerate(self.client.irun(
-            inputs=processed_inputs,
-            model=self.model,
-        )):
-            yield self._output_processing(processed_inputs[i], output)
+        # Take the first valid output, skip the rest
+        tracking_index = 0
+        for i, output in enumerate(super().irun(inputs)):
+            if tracking_index * self.max_tries <= i < (tracking_index + 1) * self.max_tries:
+                if output is not None:
+                    tracking_index += 1
+                    yield output
+                else:
+                    if i == (tracking_index + 1) * self.max_tries - 1:
+                        # All tries failed, return None
+                        tracking_index += 1
+                        yield None
 
     def irun_unordered(
         self, 
         inputs, 
     ) -> Iterator[Tuple[int, str]]:
-        processed_inputs = self._inputs_processing(inputs)
-        for i, output in self.client.irun_unordered(
-            inputs=processed_inputs,
-            model=self.model,
-        ):
-            yield i, self._output_processing(processed_inputs[i], output)
+        # Take the first valid output, skip the rest
+        tracking_index = 0
+        cached_outputs = {i: [] for i in range(len(inputs))}
+        for i, output in super().irun_unordered(inputs):
+            cached_outputs[i].append(output)
+            if i == tracking_index:
+                # Get current output
+                if output is not None:
+                    tracking_index += 1
+                    yield i, output
+                else:
+                    if len(cached_outputs[i]) >= self.max_tries:
+                        # All tries failed, return None
+                        tracking_index += 1
+                        yield i, None
 
 
 if __name__ == "__main__":
@@ -199,14 +230,14 @@ if __name__ == "__main__":
                 "properties": {
                     "name": {"type": "string"},
                     "age": {"type": "integer"},
-                    "background": {"type": "string"},
+                    "gender": {"type": "string"},
                 },
-                "required": ["name", "age", "background"],
+                "required": ["name", "age", "gender"],
             },
         },
         api_key="EMPTY",
         base_url="http://localhost:8000/v1",
-        system_prompt="Generate a list of persons with name, age, and background, that relavant to a given user input.",
+        system_prompt="Generate a list of 3 persons with name, age, and gender, that relavant to a given user input.",
     )
 
     messages = [
