af6a8360d42363c80e4f26503d8cd61f921284fa
Panuthep <falcon_270394@hotmail.co.th>
2025-09-02 18:41:11 +0800

    Update version

--- parallax_ai/__init__.py
+++ parallax_ai/__init__.py
@@ -1,6 +1,6 @@
 """Parallax - A package for parallel inference."""
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 from .clients import ParallaxOpenAIClient
 from .agents import JSONOutputAgent, KeywordOutputAgent, ClassificationAgent
