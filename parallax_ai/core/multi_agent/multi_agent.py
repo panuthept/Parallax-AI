5440990e725ab4bff44ca29b80e8b5a58b9ad75d
Panuthep <falcon_270394@hotmail.co.th>
2025-10-08 15:58:21 +0800

    MultiAgent supports dismiss_none_output=True

--- parallax_ai/core/multi_agent/multi_agent.py
+++ parallax_ai/core/multi_agent/multi_agent.py
@@ -18,16 +18,14 @@ class MultiAgent:
         max_tries: int = 1,
         dismiss_none_output: bool = False,
     ):
-        if dismiss_none_output:
-            print("Warning: dismiss_none_output is set to True. This may lead to loss of some outputs and length mismatched with inputs.")
-
         self.agents = agents
         self.agent_ios = agent_ios if agent_ios is not None else {}
         self.progress_names = progress_names if progress_names is not None else {}
         self.max_tries = max_tries
+        self.dismiss_none_output = dismiss_none_output
         self.client = client if client is not None else self.agents[list(agents.keys())[0]].client
         self.engine = ParallaxEngine(
-            client=self.client, max_tries=max_tries, dismiss_none_output=dismiss_none_output
+            client=self.client, max_tries=max_tries, dismiss_none_output=False
         )
 
         for name, agent in self.agents.items():
@@ -43,7 +41,7 @@ class MultiAgent:
         inputs: List[Tuple[str, Any]],
         progress_names: Optional[Dict[str, str]] = None,
         **kwargs,
-    ) -> Tuple[List[str], List[Any], List[str]]:
+    ) -> Tuple[List[str], List[Any], List[str], List[str]]:
         # Create jobs for all agents
         jobs = []
         agent_names = []
@@ -55,7 +53,7 @@ class MultiAgent:
             jobs.extend(agent_jobs)
             agent_names.extend(agent_name for _ in range(len(agent_jobs)))
         if len(jobs) == 0:
-            return [], [], []
+            return [], [], [], []
 
         # Shuffle jobs to mix different agents' jobs
         indices = list(range(len(jobs)))
@@ -66,6 +64,7 @@ class MultiAgent:
         # Process the jobs by the ParallaxEngine with retries
         shuffled_jobs = self.engine(shuffled_jobs, **kwargs)
         # Get outputs in the original order
+        shuffled_inputs = [job.inp for job in shuffled_jobs]
         shuffled_outputs = [job.output for job in shuffled_jobs]
         # Update conversation memory with assistant outputs
         for job, agent_name in zip(shuffled_jobs, shuffled_agent_names):
@@ -74,19 +73,29 @@ class MultiAgent:
         shuffled_session_ids = [job.session_id for job in shuffled_jobs]
 
         # Unshuffle jobs to the original order
+        inputs = [None for _ in range(len(jobs))]
         outputs = [None for _ in range(len(jobs))]
         session_ids = [None for _ in range(len(jobs))]
         agent_names = [None for _ in range(len(jobs))]
         for i, index in enumerate(indices):
+            inputs[index] = shuffled_inputs[i]
             outputs[index] = shuffled_outputs[i]
             session_ids[index] = shuffled_session_ids[i]
             agent_names[index] = shuffled_agent_names[i]
-        return agent_names, session_ids, outputs
+
+        # Dismiss None outputs (if dismiss_none_output is True)
+        if self.dismiss_none_output:
+            session_ids = [sid for sid, out in zip(session_ids, outputs) if out is not None]
+            agent_names = [an for an, out in zip(agent_names, outputs) if out is not None]
+            inputs = [inp for inp, out in zip(inputs, outputs) if out is not None]
+            outputs = [out for out in outputs if out is not None]
+        return agent_names, session_ids, inputs, outputs
 
     def _run(
         self, 
         inputs: Dict[str, Any], 
         progress_names: Optional[Dict[str, str]] = None,
+        return_inputs: bool = False,
         **kwargs
     ) -> Dict[str, Any]:
         # Use default progress names if not provided
@@ -102,15 +111,21 @@ class MultiAgent:
             inputs[agent_name], progress_names[agent_name] = self.agents[agent_name].input_transformation(inputs[agent_name], progress_names.get(agent_name, None))
 
         # Run all agents
-        agent_names, session_ids, outputs = self.__run(inputs, progress_names=progress_names, **kwargs)
+        agent_names, session_ids, inputs, outputs = self.__run(inputs, progress_names=progress_names, **kwargs)
         
         # Get outputs for each agent
         dict_outputs = defaultdict(list)
         for agent_name, session_id, output in zip(agent_names, session_ids, outputs):
             if self.agents[agent_name].conversational_agent:
-                dict_outputs[agent_name].append((session_id, output))
+                if return_inputs:
+                    dict_outputs[agent_name].append((session_id, inputs, output))
+                else:
+                    dict_outputs[agent_name].append((session_id, output))
             else:
-                dict_outputs[agent_name].append(output)
+                if return_inputs:
+                    dict_outputs[agent_name].append((inputs, output))
+                else:
+                    dict_outputs[agent_name].append(output)
 
         # Transform outputs for all agents
         for agent_name in dict_outputs.keys():
@@ -219,18 +234,21 @@ class MultiAgent:
         inputs, package_indices = self._get_pipeline_inputs(self.packages, self.agent_ios)
 
         # Execute Agents
-        outputs = self._run(deepcopy(inputs), **kwargs)
+        input_outputs = self._run(
+            inputs=deepcopy(inputs), return_inputs=True, **kwargs
+        )
 
         # Update packages with outputs
-        for agent_name, agent_outputs in outputs.items():
+        for agent_name, agent_input_outputs in input_outputs.items():
             package_index = package_indices[agent_name]
             # Process outputs if output_processing is provided
             if agent_name in self.agent_ios and self.agent_ios[agent_name].output_processing is not None:
-                agent_inputs = inputs[agent_name]
+                agent_inputs = agent_input_outputs[1] if self.agents[agent_name].conversational_agent else agent_input_outputs[0]
+                agent_outputs = agent_input_outputs[-1]
                 agent_outputs = self.agent_ios[agent_name].output_processing(
-                    deepcopy(agent_inputs),
-                    deepcopy(agent_outputs),
-                    deepcopy(self.packages[package_index].external_data)
+                    deepcopy(agent_inputs),                                 # inputs
+                    deepcopy(agent_outputs),                                # outputs
+                    deepcopy(self.packages[package_index].external_data)    # data
                 )
             self.packages[package_index].agent_outputs[agent_name] = agent_outputs
 
