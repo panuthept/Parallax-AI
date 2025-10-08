dcc9b683fb4659a52b8532e7f953d64169b26cba
Panuthep <falcon_270394@hotmail.co.th>
2025-10-08 17:49:35 +0800

    Package.id can be any string

--- parallax_ai/core/multi_agent/dataclasses.py
+++ parallax_ai/core/multi_agent/dataclasses.py
@@ -18,7 +18,7 @@ class AgentIO:
 
 @dataclass
 class Package:
-    id: UUID = field(default_factory=lambda: uuid4())
+    id: str = field(default_factory=lambda: uuid4().hex)
     agent_inputs: Dict[str, Any] = field(default_factory=dict)    # agent_name -> inputs
     agent_outputs: Dict[str, Any] = field(default_factory=dict)   # agent_name -> outputs
     external_data: Dict[str, Any] = field(default_factory=dict)      # data_name -> data_value
