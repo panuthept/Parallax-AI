a60963a23f43e8d0e76d0e3453d07c94a1f76815
Panuthep <falcon_270394@hotmail.co.th>
2025-10-17 19:28:38 +0800

    update __init__.py

--- parallax_ai/__init__.py
+++ parallax_ai/__init__.py
@@ -2,4 +2,4 @@
 
 __version__ = "0.4.0"
 
-from .core import Client, ParallaxEngine, Agent, ClassificationAgent, MultiAgent, Dependency, ModuleIO, AgentModule, FunctionModule, Package
+from .core import Client, ParallaxEngine, Agent, ClassificationAgent, MultiAgent, ModuleIO, AgentModule, FunctionModule
