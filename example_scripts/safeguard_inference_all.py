75cd15beb81516f8620ff3fc79e0f23da7c8f199
Panuthep <falcon_270394@hotmail.co.th>
2025-11-24 14:39:28 +0700

    fix bug and add performance report

--- example_scripts/safeguard_inference_all.py
+++ example_scripts/safeguard_inference_all.py
@@ -1,6 +1,8 @@
 import os
 import json
 import argparse
+import numpy as np
+from typing import List
 from parallax_ai.benchmarks import SEASafeguardBench
 from parallax_ai.builtins.safeguards import SafeguardModel, AgenticSafeguard, AgenticSafeguardMoE
 
@@ -24,6 +26,14 @@ def get_safeguard(args, worker_nodes):
     else:
         safeguard = SafeguardModel(model_name=args.model_name, worker_nodes=worker_nodes)
     return safeguard
+
+def get_result(paths: List[str], metrics: str = "pr_auc") -> float:
+    scores = []
+    for path in paths:
+        data = json.load(open(path, 'r'))
+        score = data['performance'][metrics]
+        scores.append(score)
+    return round(sum(scores) / len(scores) * 100, 1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run safeguard benchmarking.')
@@ -80,6 +90,9 @@ if __name__ == "__main__":
                     with open(f"{save_path}/prompt_classification.json", "w") as f:
                         json.dump(results, f, indent=4, ensure_ascii=False)
                 
+                if subset == "cultural_in_the_wild":
+                    continue # No response classification for this subset
+
                 if not os.path.exists(f"{save_path}/response_classification.json"):
                     results = benchmark.evaluate(
                         label_mapping={"Harmful": 1.0, "Sensitive": 1.0, "Safe": 0.0},
@@ -98,3 +111,110 @@ if __name__ == "__main__":
                         json.dump(results, f, indent=4, ensure_ascii=False)
 
     # Report performance summary
+    results = []
+    print("\n=== Performance Summary (PR-AUC) ===")
+    # Prompt Classification Results
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/general/EN/None/prompt_classification.json",
+        ]
+    ))
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/general/IN/None/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/MS/None/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/MY/None/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/TA/None/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/TH/None/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/TL/None/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/VI/None/prompt_classification.json",
+        ]
+    ))
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/IN_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/MS_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/MY_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/TA_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/TH_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/TL_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/VI_EN/English/prompt_classification.json",
+        ]
+    ))
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/IN_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/MS_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/MY_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/TA_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/TH_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/TL_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_in_the_wild/VI_EN/Local/prompt_classification.json",
+        ]
+    ))
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/IN_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/MS_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/MY_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TA_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TH_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TL_EN/English/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/VI_EN/English/prompt_classification.json",
+        ]
+    ))
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/IN_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/MS_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/MY_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TA_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TH_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TL_EN/Local/prompt_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/VI_EN/Local/prompt_classification.json",
+        ]
+    ))
+    results.append(round(np.mean(results).item(), 1))
+    response_classification_start_index = len(results)
+    # Response Classification Results
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/general/EN/None/response_classification.json",
+        ]
+    ))
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/general/IN/None/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/MS/None/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/MY/None/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/TA/None/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/TH/None/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/TL/None/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/general/VI/None/response_classification.json",
+        ]
+    ))
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/IN_EN/English/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/MS_EN/English/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/MY_EN/English/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TA_EN/English/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TH_EN/English/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TL_EN/English/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/VI_EN/English/response_classification.json",
+        ]
+    ))
+    results.append(get_result(
+        paths=[
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/IN_EN/Local/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/MS_EN/Local/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/MY_EN/Local/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TA_EN/Local/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TH_EN/Local/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/TL_EN/Local/response_classification.json",
+            f"./outputs/{save_name}/{benchmark_name}/cultural_content_generation/VI_EN/Local/response_classification.json",
+        ]
+    ))
+    results.append(round(np.mean(results[response_classification_start_index:]).item(), 1))
+
+    print(" & ".join(map(str, results)))
