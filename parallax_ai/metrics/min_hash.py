3cd6196aa081b19232962aadc831de039b62303c
Panuthep <falcon_270394@hotmail.co.th>
2025-11-26 14:02:29 +0700

    fix bug

--- parallax_ai/metrics/min_hash.py
+++ parallax_ai/metrics/min_hash.py
@@ -1,24 +1,27 @@
-import numpy as np
 from typing import List
 from random import randint
-from scipy.spatial.distance import cosine
 
 
 class MinHash:
-    def __init__(self, num_hashes: int = 128, max_value: int = 2**32 - 1, prime: int = 4294967311):
+    def __init__(self, num_hashes: int = 128, max_value: int = 2**32 - 1, prime: int = 4294967311, seed: int = None):
+        # Set random seed for reproducibility
+        if seed is not None:
+            import random
+            random.seed(seed)
+
         self.num_hashes = num_hashes
         self.max_value = max_value
         self.prime = prime
+        self.a_coeffs = [randint(0, self.max_value) for _ in range(self.num_hashes)]
+        self.b_coeffs = [randint(0, self.max_value) for _ in range(self.num_hashes)]
 
     def hash(self, input_set: set) -> List[int]:
-        hash_values = [self.max_value] * self.num_hashes
-        a_coeffs = [randint(1, self.prime - 1) for _ in range(self.num_hashes)]
-        b_coeffs = [randint(0, self.prime - 1) for _ in range(self.num_hashes)]
+        hash_values = [float("inf")] * self.num_hashes
 
         for item in input_set:
             item_hash = hash(item)
             for i in range(self.num_hashes):
-                combined_hash = (a_coeffs[i] * item_hash + b_coeffs[i]) % self.prime
+                combined_hash = (self.a_coeffs[i] * item_hash + self.b_coeffs[i]) % self.prime
                 if combined_hash < hash_values[i]:
                     hash_values[i] = combined_hash
         return hash_values
@@ -26,10 +29,5 @@ class MinHash:
     def jaccard_similarity(self, set1: set, set2: set) -> float:
         hash1 = self.hash(set1)
         hash2 = self.hash(set2)
-        identical_hashes = sum(1 for h1, h2 in zip(hash1, hash2) if h1 == h2)
+        identical_hashes = set(hash1).intersection(set(hash2)).__len__()
         return identical_hashes / self.num_hashes
-    
-    def cosine_similarity(self, set1: set, set2: set) -> float:
-        hash1 = np.array(self.hash(set1))
-        hash2 = np.array(self.hash(set2))
-        return 1 - cosine(hash1, hash2)
