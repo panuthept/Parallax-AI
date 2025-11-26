bb0df89c9d7781a6ee9d905f5849b5e19d17936b
Panuthep <falcon_270394@hotmail.co.th>
2025-11-26 17:15:53 +0800

    add enable_pipeline parameter

--- parallax_ai/core/service.py
+++ parallax_ai/core/service.py
@@ -73,44 +73,51 @@ class Service:
         self, 
         inputs: List[dict] = None, 
         instances: List[Instance] = None,
+        enable_pipeline: bool = False,  # If False, inputs are processed until finished before receiving the next inputs
         verbose: bool = True,
         debug_mode: bool = False,
     ) -> List[dict]:
         # Add inputs to DataPool
         self.datapool.add(data=inputs, instances=instances)
 
-        # Set worker nodes for all modules
-        self.update_worker_nodes(self.flattened_modules, self.worker_nodes)
-
-        # Each module creates jobs based on available data in DataPool
-        pendding_jobs: List[Job] = []
-        for module in self.flattened_modules:
-            for instance in self.datapool.retrieve(target_contents=module.interface.dependencies, avoid_modules=[module.name]):
-                pendding_jobs.extend(module.create_jobs(instance))
-
-        # Execute jobs
-        completed_jobs = self.distributor(pendding_jobs, verbose=verbose, debug_mode=debug_mode)
-
-        # Update DataPool
-        for module in self.flattened_modules:
-            jobs = [job for job in completed_jobs if job.module_name == module.name]
-            for instand_id, new_contents in module.get_output_contents(jobs).items():
-                self.datapool.update(instand_id, new_contents, executed_module=module.name)
-
         outputs = []
-        # Get composed outputs if output_composers are provided
-        if self.output_composers is not None:
-            for composer in self.output_composers:
-                for instance in self.datapool.retrieve(target_modules=composer.dependencies):
-                    if composer.condition_satisfy(instance.contents):
-                        composed_output = composer.compose(instance.contents)
-                        outputs.append(composed_output)
-                        # Clean up memory
-                        self.datapool.remove(instance.id)
-        # Get outputs from finished instances
-        for instance in self.datapool.retrieve(target_modules=[module.name for module in self.flattened_modules]):
-            output = instance.contents
-            outputs.append(output)
-            # Clean up memory
-            self.datapool.remove(instance.id)
+        while True:
+            # Set worker nodes for all modules
+            self.update_worker_nodes(self.flattened_modules, self.worker_nodes)
+
+            # Each module creates jobs based on available data in DataPool
+            pendding_jobs: List[Job] = []
+            for module in self.flattened_modules:
+                for instance in self.datapool.retrieve(target_contents=module.interface.dependencies, avoid_modules=[module.name]):
+                    pendding_jobs.extend(module.create_jobs(instance))
+
+            # Execute jobs
+            completed_jobs = self.distributor(pendding_jobs, verbose=verbose, debug_mode=debug_mode)
+
+            # Update DataPool
+            for module in self.flattened_modules:
+                jobs = [job for job in completed_jobs if job.module_name == module.name]
+                for instand_id, new_contents in module.get_output_contents(jobs).items():
+                    self.datapool.update(instand_id, new_contents, executed_module=module.name)
+
+            # Get composed outputs if output_composers are provided
+            if self.output_composers is not None:
+                for composer in self.output_composers:
+                    for instance in self.datapool.retrieve(target_modules=composer.dependencies):
+                        if composer.condition_satisfy(instance.contents):
+                            composed_output = composer.compose(instance.contents)
+                            outputs.append(composed_output)
+                            # Clean up memory
+                            self.datapool.remove(instance.id)
+            # Get outputs from finished instances
+            for instance in self.datapool.retrieve(target_modules=[module.name for module in self.flattened_modules]):
+                output = instance.contents
+                outputs.append(output)
+                # Clean up memory
+                self.datapool.remove(instance.id)
+            
+            if len(outputs) > 0:
+                break
+            if enable_pipeline:
+                break
         return outputs
