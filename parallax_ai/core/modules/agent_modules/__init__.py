83d16b845d617882f3cc2b4442094b2c0b792c46
Panuthep <falcon_270394@hotmail.co.th>
2025-11-28 21:23:53 +0700

    remove ModelSpec

--- parallax_ai/core/modules/agent_modules/__init__.py
+++ parallax_ai/core/modules/agent_modules/__init__.py
@@ -1,2 +1,2 @@
-from .agent_module import AgentModule, AgentSpec, ModelSpec
+from .agent_module import AgentModule, AgentSpec
 from .classification_module import ClassificationModule, ClassificationSpec, AgenticClassificationModule
