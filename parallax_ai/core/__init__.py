8fd04eb2687b6a0f1ddf32e0cdc49abaa01a7bfa
Panuthep <falcon_270394@hotmail.co.th>
2025-11-28 21:24:14 +0700

    remove ModelSpec

--- parallax_ai/core/__init__.py
+++ parallax_ai/core/__init__.py
@@ -5,5 +5,5 @@ from .service import Service, OutputComposer
 
 from .modules.basic_modules.lambda_module import LambdaModule
 from .modules.basic_modules.switch_module import SwitchModule
-from .modules.agent_modules.agent_module import AgentModule, AgentSpec, ModelSpec
+from .modules.agent_modules.agent_module import AgentModule, AgentSpec
 from .modules.agent_modules.classification_module import ClassificationModule, ClassificationSpec, AgenticClassificationModule
--- parallax_ai/core/modules/__init__.py
+++ parallax_ai/core/modules/__init__.py
@@ -1,4 +1,4 @@
 from .basic_modules.lambda_module import LambdaModule
 from .basic_modules.switch_module import SwitchModule
-from .agent_modules.agent_module import AgentModule, AgentSpec, ModelSpec
+from .agent_modules.agent_module import AgentModule, AgentSpec
 from .agent_modules.classification_module import ClassificationModule, ClassificationSpec, AgenticClassificationModule
