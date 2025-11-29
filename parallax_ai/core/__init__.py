883a619555839c88dab57ca4080fda6791ac994f
Panuthep <falcon_270394@hotmail.co.th>
2025-11-29 14:36:08 +0700

    add Job and Instance

--- parallax_ai/core/__init__.py
+++ parallax_ai/core/__init__.py
@@ -1,4 +1,5 @@
 from .datapool import DataPool
 from .distributor import Distributor
+from .dataclasses import Job, Instance
 from .module import Module, ModuleInterface
 from .service import Service, OutputComposer
