72d94063805b51585bddbd2f50318f1eba1cfe0f
Panuthep <falcon_270394@hotmail.co.th>
2025-12-03 16:28:55 +0700

    Add initial codes

---
+++ parallax_ai/tools/analysis/__init__.py
@@ -0,0 +1,1 @@
+from .word_bias_analysis import WordBiasAnalysis
---
+++ parallax_ai/tools/analysis/word_bias_analysis.py
@@ -0,0 +1,17 @@
+from typing import List, Tuple, Dict
+
+
+class WordBiasAnalysis:
+    def __init__(self, training_samples: List[Tuple[List[str], str]]):
+        self.training_samples = training_samples
+
+    def analyze_bias(self) -> Dict[str, List[Tuple[str, float]]]:
+        pass
+
+
+if __name__ == "__main__":
+    training_samples = [
+        {"inputs": ["Is", "41", "a", "prime", "number", "?"], "label": "Safe"},
+        {"inputs": ["How", "to", "make", "a", "bomb", "?"], "label": "Harmful"},
+    ]
+    tool = WordBiasAnalysis(training_samples)
