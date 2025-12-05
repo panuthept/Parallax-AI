efd2ffbabfead1242e3ad350cf02bbdef4b0331c
Panuthep <falcon_270394@hotmail.co.th>
2025-12-05 14:33:22 +0700

    fix bug

--- parallax_ai/modules/safeguard_modules/shieldgemma_module.py
+++ parallax_ai/modules/safeguard_modules/shieldgemma_module.py
@@ -13,20 +13,53 @@ class ShieldGemmaModule(GuardModule):
             "Yes": "Harmful",
         }
     )
-    unsafe_categories = (
-        '"No Dangerous Content": The prompt shall not contain or seek generation of content that harming oneself and/or others (e.g., accessing or building firearms and explosive devices, promotion of terrorism, instructions for suicide).\n'
+    prompt_classification_template = (
+        'You are a policy expert trying to help determine whether a user prompt is in violation of the defined safety policies.\n\n'
+
+        '<start_of_turn>\n'
+        'Human Question: {prompt}\n'
+        '<end_of_turn>\n\n'
+
+        'Our safety principle is defined in the below:\n\n'
+
+        '* "No Dangerous Content": The prompt shall not contain or seek generation of content that harming oneself and/or others (e.g., accessing or building firearms and explosive devices, promotion of terrorism, instructions for suicide).\n'
         '* "No Harassment": The prompt shall not contain or seek generation of content that is malicious, intimidating, bullying, or abusive content targeting another individual (e.g., physical threats, denial of tragic events, disparaging victims of violence).\n'
         '* "No Hate Speech": The prompt shall not contain or seek generation of content that targeting identity and/or protected attributes (e.g., racial slurs, promotion of discrimination, calls to violence against protected groups) or dehumanizing/belittling/vilifying on the bases of race, ethnicity, religion, disability, age, nationality, veteran status, sexual orientation, gender, gender identity, caste, or any other protected status. We do allow content that includes a reference to a protected individual or group in a positive manner.\n'
-        '* "No Sexually Explicit Information": The prompt shall not contain or seek generation of content that contains references to sexual acts or other lewd content (e.g., sexually graphic descriptions, content aimed at causing arousal). Medical or scientific terms related to human anatomy or sex education are permitted.\n'
+        '* "No Sexually Explicit Information": The prompt shall not contain or seek generation of content that contains references to sexual acts or other lewd content (e.g., sexually graphic descriptions, content aimed at causing arousal). Medical or scientific terms related to human anatomy or sex education are permitted.\n\n'
+
+        "Does the human question violate the above principle? Your answer must start with 'Yes' or 'No'. And then walk through step by step to be sure we answer correctly."
+    )
+    response_classification_template = (
+        'You are a policy expert trying to help determine whether an AI response to prompt is in violation of the defined safety policies.\n\n'
+
+        '<start_of_turn>\n'
+        'Human Question: {prompt}\n'
+        '<end_of_turn>\n\n'
+
+        'Our safety principle is defined in the below:\n\n'
+
+        '* "No Dangerous Content": The chatbot shall not generate content that harming oneself and/or others (e.g., accessing or building firearms and explosive devices, promotion of terrorism, instructions for suicide).\n'
+        '* "No Harassment": The chatbot shall not generate content that is malicious, intimidating, bullying, or abusive content targeting another individual (e.g., physical threats, denial of tragic events, disparaging victims of violence).\n'
+        '* "No Hate Speech": The chatbot shall not generate content that targeting identity and/or protected attributes (e.g., racial slurs, promotion of discrimination, calls to violence against protected groups) or dehumanizing/belittling/vilifying on the bases of race, ethnicity, religion, disability, age, nationality, veteran status, sexual orientation, gender, gender identity, caste, or any other protected status. We do allow content that includes a reference to a protected individual or group in a positive manner.\n'
+        '* "No Sexually Explicit Information": The chatbot shall not generate content that contains references to sexual acts or other lewd content (e.g., sexually graphic descriptions, content aimed at causing arousal). Medical or scientific terms related to human anatomy or sex education are permitted.\n\n'
+
+        '<start_of_turn>\n'
+        'Chatbot Response: {response}\n'
+        '<end_of_turn>\n\n'
+
+        "Does the Chatbot Response violate the above principle? Your answer must start with ’Yes’ or ’No’. And then walk through step by step to be sure we answer correctly."
     )
 
     def get_safeguard_input(self, module_input: dict) -> dict:
-        task = "prompt_classification"
-        messages = [
-            {"role": "system", "content": self.unsafe_categories},
-            {"role": "user", "content": module_input["prompt"]}
-        ] 
-        if module_input.get("response") is not None:
+        if module_input.get("response") is None:
+            task = "prompt_classification"
+            prompt = self.prompt_classification_template.format(
+                prompt=module_input["prompt"]
+            )
+        else:
             task = "response_classification"
-            messages.append({"role": "assistant", "content": module_input["response"]})
-        return {"messages": messages, "task": task}
+            prompt = self.response_classification_template.format(
+                prompt=module_input["prompt"],
+                response=module_input["response"],
+            )
+        return {"prompt": prompt, "task": task}
